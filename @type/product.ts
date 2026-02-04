a4133c91f55056ee3ead89327404c3b4b7717c50
Chitam212 <chitam2122004@gmail.com>
2026-02-04 12:51:10 +0700

    Update product.ts

--- @type/product.ts
+++ @type/product.ts
@@ -9,3 +9,23 @@ export interface Product {
   images: string[] | string;
   category_id?: number | string;
 }
+
+export interface Review {
+  id?: number | string;
+  user_id: number | string;
+  product_id: number | string;
+  rating: number; // 1-5
+  comment: string;
+  created_at?: string;
+  is_anonymous?: boolean;
+  has_purchased?: boolean;
+  media_urls?: string[]; // images and videos
+  username?: string;
+}
+
+export interface ReviewRequest {
+  rating: number;
+  comment: string;
+  is_anonymous?: boolean;
+  media_urls?: string[];
+}
